165a0ed155308175186f7d6c5458ec266efcbeca
Muhammad Hamza Khan <hamza.khansahab@gmail.com>
2021-05-28 16:13:55 +0500

    time functionlaty added

--- src/components/card.js
+++ src/components/card.js
@@ -1,71 +1,97 @@
 import "./../styles/card.css";
-import CountUp from 'react-countup';
-import React from "react";
+import CountUp from "react-countup";
+import React, { useState, useEffect } from "react";
 
-// const clock = (props,time)=>{
-//     const propsData = props.time;
-//     if(propsData){
-//     const tempTime = {...time};
-//     const meridium = propsData.split(" ")[1];
-//     const splittedTime = (propsData.split(" ")[0]).split(":");
-//     var intTime = splittedTime.map((element)=>{
-//         return parseInt(element);
-//     })
+const objConverter = (time) => {
+  const propsData = time;
+  const meridium = propsData.split(" ")[1];
+  const splittedTime = propsData.split(" ")[0].split(":");
+  var intTime = splittedTime.map((element) => {
+    return parseInt(element);
+  });
 
-//     intTime[2] = intTime[2]++;
-//     if(intTime[2] >= 60){
-//         intTime[1] = intTime[1]++;
-//         if(intTime[1] >= 60){
-//             intTime[0] = intTime[0]++;
-//         }
-//     }
+  const tempTime = {
+    hour: intTime[0],
+    min: intTime[1],
+    sec: intTime[2],
+    meridium: meridium,
+  };
+
+  return tempTime;
+};
+
+const clock = (obj) => {
+  const temp = { ...obj };
+
+  temp.sec += 1;
+  if (temp.sec >= 60) {
+    temp.sec = 0;
+    temp.min += 1;
+    if (temp.min >= 60) {
+      temp.min = 0;
+      temp.hour += 1;
+    }
+  }
+  return temp;
+};
+
+const Card = (props) => {
+  const [time, setTime] = useState({
+    hour: 0,
+    min: 0,
+    sec: 0,
+    meridium: "AM",
+  });
+
+  useEffect(() => {
+    const converted = objConverter(props.time);
+    const obj1 = clock(converted);
+    setTime(obj1);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const obj2 = clock(time);
+      setTime(obj2);
+    }, 1000);
     
-//     tempTime.hour = intTime[0];
-//     tempTime.sec = intTime[1];
-//     tempTime.min = intTime[2];
-//     tempTime.meridium = meridium;
+  }, [time]);
 
-//     return tempTime;
-//     }
-    
-// }
-
-const Card = (props)=>{
-
-    // const [time, setTime] = useState({
-    //         hour : 0,
-    //         min : 0,
-    //         sec: 0,
-    //         meridium : "AM"
-    // })
-
-    // const obj = clock(props, time);
-    // setTime(obj);
-
-    // {
-    //     time.hour.toString() +" : "+ 
-    //     time.min.toString() + " : " + 
-    //     time.sec.toString() +" "+
-    //     time.meridium
-    // }
-
-
-    return(
-        <div className = "card">
-            <div className="card-content">
-                <div className="title">{props.data.name}</div>
-                <div>{props.time}</div>
-                <div>{props.date}</div>
-            </div>
-            <div className="temparature"><CountUp className="count" start={0} end={props.data.main.temp} duration={2} delay={0.5}/> &deg;C</div>
-            <div style={{"textAlign": "center"}}>{props.data.weather[0].main}</div>
-            <div className="bottom-tiles">
-                <div className="tile">Feels Like : {props.data.main.feels_like}&deg; C</div>
-                <div className="tile">Humidity : {props.data.main.humidity}%</div>
-                <div className="tile">Wind : {props.data.wind.speed} m/s</div>
-            </div>
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="title">{props.data.name}</div>
+        <div>
+          {time.hour.toString() +
+            " : " +
+            time.min.toString() +
+            " : " +
+            time.sec.toString() +
+            " " +
+            time.meridium}
         </div>
-    );
-}
+        <div>{props.date}</div>
+      </div>
+      <div className="temparature">
+        <CountUp
+          className="count"
+          start={0}
+          end={props.data.main.temp}
+          duration={2}
+          delay={0.5}
+        />{" "}
+        &deg;C
+      </div>
+      <div style={{ textAlign: "center" }}>{props.data.weather[0].main}</div>
+      <div className="bottom-tiles">
+        <div className="tile">
+          Feels Like : {props.data.main.feels_like}&deg; C
+        </div>
+        <div className="tile">Humidity : {props.data.main.humidity}%</div>
+        <div className="tile">Wind : {props.data.wind.speed} m/s</div>
+      </div>
+    </div>
+  );
+};
 
-export default React.memo(Card);
+export default Card;
